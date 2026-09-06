6b6c8a0eceb9433af48a89d4dd94dd565f4d269c
Roman <hozgllox@gmail.com>
2026-09-07 00:11:52 +0300

    lab2

---
+++ lab2/Valuator/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace Valuator;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddSingleton( sp =>
+        {
+            var configuration = builder.Configuration.GetConnectionString( "Redis" );
+            var redis = ConnectionMultiplexer.Connect( configuration );
+            return redis.GetDatabase();
+        } );
+
+        builder.Services.AddRazorPages( options =>
+        {
+            options.Conventions.ConfigureFilter( new IgnoreAntiforgeryTokenAttribute() );
+        } );
+
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+        }
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
+}
